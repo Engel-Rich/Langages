8be3714f8edac1d6b2b0144658c8147bd0abadc2
ivan878 <ivankouamou21@gmail.com>
2023-07-26 15:39:04 +0200

    initiale_p1

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -27,8 +27,8 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           home: UserProvider.user.tokenuser == null
-              ? const HomePage()
-              : const InitialScreen(),
+              ? const InitialScreen()
+              : const HomePage(),
         );
       },
     );
--- mobile/lib/views/sreens/initialpage.dart
+++ mobile/lib/views/sreens/initialpage.dart
@@ -4,6 +4,7 @@ import 'package:mobile/views/components/button.dart';
 import 'package:mobile/views/sreens/loginsreen.dart';
 import 'package:mobile/views/sreens/register.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -15,6 +16,14 @@ class InitialScreen extends StatefulWidget {
 class _InitialScreenState extends State<InitialScreen> {
   late Material materialButton;
   late int index;
+  List imageList = [
+    {"id": 1, "image_path": 'assets/image1.png'},
+    {"id": 2, "image_path": 'assets/image2.jpg'},
+    {"id": 3, "image_path": 'assets/image3.png'},
+    {"id": 4, "image_path": 'assets/image4.png'},
+  ];
+  final CarouselController carouselController = CarouselController();
+  int currentindex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +33,30 @@ class _InitialScreenState extends State<InitialScreen> {
           padding: const EdgeInsets.all(12.0),
           child: Column(
             children: [
-              Flexible(flex: 7, child: Container()),
+              Flexible(
+                flex: 7,
+                child: CarouselSlider(
+                  items: imageList
+                      .map((data) => Image.asset(
+                            data['image_path'],
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200,
+                          ))
+                      .toList(),
+                  carouselController: carouselController,
+                  options: CarouselOptions(
+                      scrollPhysics: const BouncingScrollPhysics(),
+                      autoPlay: true,
+                      aspectRatio: 2,
+                      viewportFraction: 1,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentindex = index;
+                        });
+                      }),
+                ),
+              ),
               Flexible(
                   flex: 3,
                   child: SizedBox(
